7fff069e0cab46f5d077f13a2ddd25ba553ffc0f
dernathul <lautaroparracab@gmail.com>
2020-04-02 16:06:12 +0200

    article functionality added but still not presenting, just the logic

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardContent, IonItem, IonIcon, IonLabel, IonButton } from '@ionic/react';
-import { basketball } from 'ionicons/icons';
+import React from "react";
+import { connect } from "react-redux";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonLabel,
+  IonButton
+} from "@ionic/react";
+import { basketball } from "ionicons/icons";
 
-const CardExamples = props => {
-  
+const DisplayArticles = props => {
+  let articleDisplay = props.articles.map(article => {
+    return (
+      <IonPage>
+        <IonContent>
+          <IonCard>
+            <IonItem key={article.id} id={"article-" + article.id}>
+              <IonIcon icon={basketball} slot="start" />
+              <IonLabel id="title">{article.title}</IonLabel>
+              <IonButton fill="outline" slot="end">
+                View
+              </IonButton>
+            </IonItem>
+            <IonCardContent id="snippet">{article.snippet}</IonCardContent>
+          </IonCard>
+        </IonContent>
+      </IonPage>
+    );
+  });
   return (
-    <IonPage>
-      <IonContent>
-        <IonCard>
-          <IonItem>
-            <IonIcon icon={basketball} slot="start" />
-            <IonLabel>Title snippet</IonLabel>
-            <IonButton fill="outline" slot="end">View</IonButton>
-          </IonItem>
-          <IonCardContent>
-            This is some snippet
-      </IonCardContent>
-        </IonCard>
-      </IonContent>
-    </IonPage>
-  );
+  <>
+  {articleDisplay}
+  </>
+  )
+};
+const mapStateToProps = state => {
+  return {
+
+    articles: state.articles
+  }
 }
 
-export default CardExamples
+export default connect(mapStateToProps)(DisplayArticles);
