90ffb72db9f91da0c43802d09c9636dbe4e98d57
dernathul <lautaroparracab@gmail.com>
2020-04-02 20:10:20 +0200

    componenty for single article added, u´´but still not rendering

---
+++ src/components/DisplaySingleArticle.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { connect } from "react-redux";
+import { BACK_TO_ARTICLES_LIST } from "../state/actions/actionTypes";
+import {
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonItem,
+  IonIcon,
+  IonButton,
+  IonGrid,
+  IonTitle,
+  IonSegment
+} from "@ionic/react";
+
+import React from "react";
+
+const DisplaySingleArticle = props => {
+  let articleDetails;
+  let article = props.singleArticle;
+  articleDetails = (
+    <IonCard>
+      <IonGrid>
+        <IonItem>
+          <IonTitle key={article.title}>{article.title}</IonTitle>
+        </IonItem>
+        <img src={article.image} alt={`${article.title}-image`} />
+        <IonItem>
+          <IonCardContent key={article.snippet}>
+            {article.snippet}
+          </IonCardContent>
+        </IonItem>
+        <IonItem>
+          <IonCardContent key={article.content}>
+            {article.content}
+          </IonCardContent>
+        </IonItem>
+        <IonItem>
+          <IonButton onClick={() => props.dispatch({type: BACK_TO_ARTICLES_LIST})}>back</IonButton>
+        </IonItem>
+      </IonGrid>
+    </IonCard>
+  );
+
+return <IonSegment>{articleDetails}</IonSegment>;
+};
+
+const mapStateToProps = state => {
+  return {
+    singleArticle: state.singleArticle 
+  }
+
+}
+export default connect(mapStateToProps)(DisplaySingleArticle);
