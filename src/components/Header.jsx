20fc092abeb8301007cacd6e943e4c74b9b11bbd
Carlos <carlos.delgado@hotmail.it>
2020-04-02 09:44:53 +0200

    adding basic scaffolding content, clean up file

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonContent,
+  IonMenu,
+  IonList,
+  IonItem,
+  IonSegment
+} from '@ionic/react';
+
+
+
+const Header = () => {
+  return (
+    <IonSegment>
+  Menu Item
+    </IonSegment>
+  )
+}
+export default Header
