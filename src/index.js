3981294c4338b2488376843874b2b7750562a62a
dernathul <lautaroparracab@gmail.com>
2020-04-02 15:41:17 +0200

    heroku link to api added and article action created

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import configureStore from "./state/store/configureStore";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://newsroom3api.herokuapp.com/api/v1/";
 
 const store = configureStore();
 window.store = store;
---
+++ src/state/actions/articleAction.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { GET_ARTICLE_DATA } from "./actionTypes";
+
+const apiURL = "https://newsroom3api.herokuapp.com/api/v1/articles";
+
+const fetchArticles = () => {
+  return async dispatch => {
+    let response = await axios.get(apiURL);
+    return dispatch(dispatchArticleAction(response.data));
+  };
+};
+
+const dispatchArticleAction = json => {
+  return { type: GET_ARTICLE_DATA, payload: json };
+};
+
+export { fetchArticles };
