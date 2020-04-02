d5652806c0a636fea04107eddbe832c9f0d49e28
Janko <mailodjanka@gmail.com>
2020-04-02 19:33:41 +0200

    Defines fetchSingleArticle method in article actions

--- src/state/actions/articleAction.js
+++ src/state/actions/articleAction.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ARTICLE_DATA } from "./actionTypes";
+import { GET_ARTICLE_DATA, GET_SINGLE_ARTICLE_DATA } from "./actionTypes";
 
 const apiURL = "https://newsroom3api.herokuapp.com/api/v1/articles";
 
@@ -10,8 +10,19 @@ const fetchArticles = () => {
   };
 };
 
+const fetchSingleArticle = id => {
+  return async dispatch => {
+    let response = await axios.get(`/articles/${id}`);
+    return dispatch(dispatchSingleArticleAction(response.data));
+  };
+};
+
 const dispatchArticleAction = json => {
   return { type: GET_ARTICLE_DATA, payload: json };
 };
 
-export { fetchArticles };
+const dispatchSingleArticleAction = json => {
+  return { type: GET_SINGLE_ARTICLE_DATA, payload: json};
+}
+
+export { fetchArticles, fetchSingleArticle };
